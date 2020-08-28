c8594da6a371c77fb7a5606942914131d8f12028
kendreaditya <kendreaditya@gmail.com>
2020-08-27 23:38:55 -0400

    :art: feat: Data saves as TensorDataset (torch)

--- data_segmentation.py
+++ data_segmentation.py
@@ -1,28 +1,25 @@
-import sqlite3
+import torch
+from torch.utils.data import TensorDataset
 from tqdm import tqdm
 from mat73 import loadmat
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.io as sio
 import os
 
 def segmentation(arr, output_shape):
     output_shape.append(-1)
     return np.average(np.reshape(arr, output_shape), axis=-1)
 
-PATH = "../data/"
+PATH = "../data/raw-data/"
 data_files = os.listdir(PATH)
-
-conn = sqlite3.connect(PATH+"data.db")
-database = conn.cursor()
-database.execute("""CREATE TABLE data (
-                 twoD text)""")
+dataset = {'X':[], 'y':[]}
 
 for fn in data_files:
     labels = loadmat(PATH+fn)["label"]
     data = loadmat(PATH+fn)["data"]
     for datum, label in tqdm(zip(data, labels)):
         datum = segmentation(datum[0], [34, 34])
-        database.execute("INSERT INTO data VALUES (:twoD)", {'twoD': repr([datum, label])})
-        conn.commit()
-conn.close()
+        dataset['X'].append(datum)
+        dataset['y'].append(label)
+
+torch.save(TensorDataset(torch.Tensor(dataset['X']), torch.Tensor(dataset['y'])), '../data/twoD_data.pt')
