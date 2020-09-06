0362602bc2f8fd33c4f342161953444ea70949b5
kendreaditya <kendreaditya@gmail.com>
2020-09-05 22:24:22 -0400

    :sparkles: feat: Added forward method for Train class

--- models/densenet.py
+++ models/densenet.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from train import Train
 
 densenet = models.densenet161(pretrained=True)
 
--- models/train.py
+++ models/train.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 
+#Precison, Specifity/Sensitivity, Recall, F1, ROC curve,
 # ADD SOUPORT FOR TPUs
 class Train():
     def __init__(self, model, dataset, epochs, batch_size, criterion, optimizer, cross_validation, device='cuda'):
@@ -27,34 +28,75 @@ class Train():
     def cuda_device(self):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def logger(self, epoch, train_loss, train_acc, val_loss, val_acc):
+    def logger(self, epoch, batch, train_loss, train_acc, val_loss, val_acc):
         if os.path.exists(f"../model-information/{self.log_name}"):
             with open(f"../model-information/{self.log_name}", 'a') as file:
                 file.write(str(self.criterion))
                 file.write(str(self.optimizer))
-                file.wrie("epochs,training loss,training accuracy,validation loss,validation accuracy\n")
+                file.wrie("epochs,batch,training loss,training accuracy,validation loss,validation accuracy\n")
 
         with open(f"../model-information/{self.log_name}", 'a') as file:
-            file.write(f"{epoch},{round(float(train_loss),4)},{round(float(train_acc),4)},{round(float(val_loss),4)},{round(float(val_acc),4)}\n")
+            file.write(f"{epoch},{batch},{round(float(train_loss),4)},{round(float(train_acc),4)},{round(float(val_loss),4)},{round(float(val_acc),4)}\n")
 
     def data_loader(self, dataset, batch_size, cross_validation):
         train_set, val_set, test_set = torch.utils.data.random_split(dataset, lengths=cross_validation)
         self.train_dataloader = torch.utils.data.DataLoader(train_set, batch_size, shuffle=True)
-        self.val_dataloader = torch.utils.data.DataLoader(val_set, batch_size, shuffle=True)
-        self.test_set = torch.utils.data.DataLoader(test_set, batch_size, shuffle=True)
+        self.val_dataloader = torch.utils.data.DataLoader(val_set, batch_size)
+        self.test_set = torch.utils.data.DataLoader(test_set, batch_size)
 
     def train(self):
-        train_losses, val_losses = [], []
-        train_accs, val_accs = [], []
-
         for epoch in epochs:
-            for batch_i,(data, target) in enumerate(self.train_dataloader):
-                self.model.train()
+            for batch_i,(data, targets) in enumerate(self.train_dataloader):
+                # Training
+                data.to(self.device)
+                target.to(self.device)
 
-            else:
-                self.model.eval()
+                train_loss, train_acc = forward_pass(data, targets, train=True)
 
+                # Validation
+                val_accs, val_losses = 0,0
+                for val_batch_i, (val_data, val_targets) in enumerate(self.val_dataloader):
+                    val_data.to(self.device)
+                    val_targets.to(self.device)
+                    val_loss, val_acc = forward_pass(val_data, val_targets, train=False)
+                    val_accs += val_acc
+                    val_losses += val_loss
+                val_accs, val_losses = val_accs/val_batch_i, val_losses/val_batch_
+
+                # Logs model metrics
+                self.logger(epoch, batch_i, train_loss, train_acc, val_losses, val_acces)
+
+    def forward_pass(self, data, targets, train=False):
+        if train:
+            self.model.zero_grad()
+            self.model.train()
+        else:
+            self.model.eval()
+
+        outputs = self.model(data)
+        matches = [torch.argmax(i)==torch.argmax(j) for i,j in zip(outputs, targets)]
+        loss = self.criterion(outputs, targets)
+        acc = matches.count(True)/len(matches)
+
+        if train:
+            loss.backward()
+            optimizer.step()
+
+        del outputs
+        del matches
+        return loss.item(), acc
 
 
     def test(self):
+        accs, losses = 0,0
+        for batch_i, (data, targets) in enumerate(self.test_set):
+            data.to(self.device)
+            targets.to(self.device)
+            loss, acc = forward_pass(data, targets, train=False)
+            accs += acc
+            losses += loss
+        accs, losses = accs/batch_i, losses/batch_i
+        return accs, losses
+
+    def graph(self):
         pass
