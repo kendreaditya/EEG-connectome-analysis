ce10078e9f5c414d4fa958f4b70ad6bb40fb7be8
kendreaditya <kendreaditya@gmail.com>
2020-09-27 14:20:52 -0400

    :lipstick: style: removed test code in Encoder2DCNN module

--- models/Encoder2DCNN.py
+++ models/Encoder2DCNN.py
@@ -7,7 +7,7 @@ class AutoEncodeCNN(nn.Module):
         super(AutoEncodeCNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.pool = nn.MaxPool2d(kernel_size=(2,2))
         self.transconv1 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=(2,2), stride=2, padding=0)
         self.transconv2 = nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=(3, 3), stride=1, padding=1)
 
@@ -30,7 +30,7 @@ class EncoderCNN(nn.Module):
         super(EncoderCNN, self).__init__()
         # Encoder
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), stride=1, padding=1)
-        self.conv1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=(2, 2))
 
         # Dense Layers
@@ -45,9 +45,3 @@ class EncoderCNN(nn.Module):
         X = F.relu(self.fc1(X))
         X = self.fc2(X)
         return X
-
-model = AutoEncodeCNN()
-data = torch.rand(33, 1, 34, 34)
-print(data.shape)
-outputs = model(data)
-print(outputs.shape)
