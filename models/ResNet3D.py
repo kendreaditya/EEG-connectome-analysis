4176864dafeef6a79af68e765b8fd88b1c2db605
kendreaditya <kendreaditya@gmail.com>
2020-10-10 19:06:47 -0400

    :bug: fix: dynamically change fc input size based on data shape

--- models/ResNet3D.py
+++ models/ResNet3D.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Resnet4(nn.Module):
-    def __init__(self, in_channel):
+    def __init__(self, input_size, in_channel):
         super(Resnet4, self).__init__()
         self.layer0 = nn.Sequential(nn.Conv3d(in_channel, 32, kernel_size=(4, 4, 4), stride=(2, 2, 2)),
                                     nn.BatchNorm3d(32),
@@ -14,7 +14,9 @@ class Resnet4(nn.Module):
                                     nn.BatchNorm3d(64),
                                     nn.ReLU())
 
-        self.fc1 = nn.Sequential(nn.Linear(3*3*2*64, 256),
+        conv_output = self.conv_output(torch.zeros(input_size))
+
+        self.fc1 = nn.Sequential(nn.Linear(conv_output[-1], 256),
                                  nn.ReLU())
         self.fc2 = nn.Sequential(nn.Linear(256, 3))
 
@@ -30,3 +32,8 @@ class Resnet4(nn.Module):
         X = self.fc1(X)
         X = self.fc2(X)
         return X
+
+    def conv_output(self, X):
+        X = self.layer0(X)
+        X = self.layer1(X)
+        return X.flatten().shape
